770d5318ca7ae08cc2a2ba3fb0eb911e6f313bf0
karimnroses <karimderrouiche2@gmail.com>
2022-07-06 22:09:48 +0200

    add Admin updateUserInfos && updateUserPassword

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,41 +1,52 @@
 import pool from "../db/pg.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import {addUserValidation, addCompanyValidation} from "../validation.js";
+import { addUserValidation, addCompanyValidation } from "../validation.js";
 
 /*********************___Create a New Company__*************************/
 export const createNewCompany = async (req, res) => {
-   // Validate Admin Input for a Company
-   const { error } = addCompanyValidation(req.body);
-   if (error) return res.status(400).send(error.details[0].message);
+  // Validate Admin Input for a Company
+  const { error } = addCompanyValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const status_id = 1; //Standards status for new created company is "aktiv" -> ID = 1. take a look at the company_status
-    const { company_name, adress, number, zip, city, country, phone, email} = req.body;
-    await pool
-      .query(
-        `
+  const status_id = 1; //Standards status for new created company is "aktiv" -> ID = 1. take a look at the company_status
+  const { company_name, adress, number, zip, city, country, phone, email } =
+    req.body;
+  await pool
+    .query(
+      `
       SELECT * FROM company WHERE name = $1;
       `,
-        [company_name]
-      )
-      .then((company) => {
-        if (company.rowCount > 0) {
-          res.status(409).json("Company already exists");
-        } else {
-          pool
-            .query(
-              `
+      [company_name]
+    )
+    .then((company) => {
+      if (company.rowCount > 0) {
+        res.status(409).json("Company already exists");
+      } else {
+        pool
+          .query(
+            `
           INSERT INTO company 
           (name, adress, number, zip, city, country, status_id, phone, email)
           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;
           `,
-              [company_name, adress, number, zip, city, country, status_id, phone, email]
-            )
-            .then((result) => res.status(201).json(result))
-            .catch((err) => res.json({ err: err.message }));
-        }
-      })
-      .catch((err) => res.json({ err: err.message }));
+            [
+              company_name,
+              adress,
+              number,
+              zip,
+              city,
+              country,
+              status_id,
+              phone,
+              email,
+            ]
+          )
+          .then((result) => res.status(201).json(result))
+          .catch((err) => res.json({ err: err.message }));
+      }
+    })
+    .catch((err) => res.json({ err: err.message }));
 };
 
 /*********************___Create a New User___*************************/
@@ -51,7 +62,7 @@ export const createNewUser = async (req, res) => {
     last_name,
     username,
     company_name,
-    role
+    role,
   } = req.body;
 
   let company_id;
@@ -106,7 +117,7 @@ export const createNewUser = async (req, res) => {
                   username,
                   company_id,
                   role_id,
-                  status_id
+                  status_id,
                 ]
               )
               .then((user) => {
@@ -131,57 +142,16 @@ export const createNewUser = async (req, res) => {
     });
 };
 
-/*********************___Delete User ___**************************/ 
+/*********************___Delete User ___**************************/
 export const updateCompanysStatus = async (req, res) => {
   const { company_id } = req.params;
-  const { new_status} = req.body;
- 
-  if(typeof new_status === 'number'){
+  const { new_status } = req.body;
+
+  if (typeof new_status === "number") {
     await pool
-    .query(`UPDATE company SET status_id = $1 WHERE id = $2 RETURNING *;`, [
-      new_status,
-      company_id,
-    ])
-    .then((results) => {
-      res.status(201).json(results);
-    })
-    .catch((err) => {
-      res.status(500).json({ err: err.message });
-    })
-  } else {
-    res.status(500).json("The given status is not a number")
-  }
- 
-};
-  
-/*********************___Get the list of all the companies___*************************/
-export const getAllCompanies = async (req, res) => {
-    await pool.query(
-      `SELECT  u.id AS "user_id", c.id AS "Company_id",  name AS "company_Name", u.first_name, u.last_name, u.email, u.username FROM users u
-      INNER JOIN company c
-      ON u.company_id = c.id
-      ORDER BY c.name ASC;`)
-      .then((result)=>{
-        res.status(200).json(result);
-      })
-      .catch((error) => { res.status(500).json({ error: error.message })}) 
-};
-
-
-/*********************___Update existing Ticket ___*************************/
-export const updateTicket = async (req, res) => {
-  const { new_ticket_status_id } = req.body;
-  const { ticket_id } = req.params;
-   /*
-      Hier soll im Frontend Wenn admin einen Status gewählt hat nur die ID des gewählten Status in die Request geschickt werden
-      Zum Beispiel wenn admin den Status "open" wählt, soll der Value des dropdown button 1 sein und nicht "open"
-      Bitte in die Tabelle ticketit_status die ID´s herausinden für jeden Status.
-      Der ticket_id wird mit jedem Ticket zurückgeliefert => getCompanyTickets()
-  */
-     pool
-      .query(`UPDATE ticketit SET status_id = $1 WHERE id = $2 RETURNING *;`, [
-        new_ticket_status_id,
-        ticket_id
+      .query(`UPDATE company SET status_id = $1 WHERE id = $2 RETURNING *;`, [
+        new_status,
+        company_id,
       ])
       .then((results) => {
         res.status(201).json(results);
@@ -189,6 +159,50 @@ export const updateTicket = async (req, res) => {
       .catch((err) => {
         res.status(500).json({ err: err.message });
       });
+  } else {
+    res.status(500).json("The given status is not a number");
+  }
+};
+
+/*********************___Get the list of all the companies___*************************/
+export const getAllCompanies = async (req, res) => {
+  await pool
+    .query(
+      `SELECT  u.id AS "user_id", c.id AS "Company_id",  name AS "company_Name", u.first_name, u.last_name, u.email, u.username FROM users u
+      INNER JOIN company c
+      ON u.company_id = c.id
+      ORDER BY c.name ASC;`
+    )
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+};
+
+/*********************___Update existing Ticket ___*************************/
+export const updateTicket = async (req, res) => {
+  const { new_ticket_status_id } = req.body;
+  const { ticket_id } = req.params;
+  /*
+      Hier soll im Frontend Wenn admin einen Status gewählt hat nur die ID des gewählten Status in die Request geschickt werden
+      Zum Beispiel wenn admin den Status "open" wählt, soll der Value des dropdown button 1 sein und nicht "open"
+      Bitte in die Tabelle ticketit_status die ID´s herausinden für jeden Status.
+      Der ticket_id wird mit jedem Ticket zurückgeliefert => getCompanyTickets()
+  */
+
+  pool
+    .query(`UPDATE ticketit SET status_id = $1 WHERE id = $2 RETURNING *;`, [
+      new_ticket_status_id,
+      ticket_id,
+    ])
+    .then((results) => {
+      res.status(201).json(results);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err.message });
+    });
 };
 
 /*********************___Get All the Ticket from one User___*************************/
@@ -196,9 +210,9 @@ export const getCompanyTickets = async (req, res) => {
   const { company_id } = req.params;
   console.log(company_id);
 
-    await pool
-      .query(
-        `
+  await pool
+    .query(
+      `
     SELECT t.id, t.subject ,c.name, u.username
     FROM company c
     JOIN users u
@@ -208,27 +222,27 @@ export const getCompanyTickets = async (req, res) => {
     WHERE c.id = $1
     ORDER BY c.name ASC
     `,
-        [company_id]
-      )
-      .then((result) => {
-        if (result.rowCount == 0) {
-          res.status(404).json("The selected company has no Tickets");
-        } else {
-          res.status(200).json(result);
-        }
-      })
-      .catch((error) =>  {res.status(500).json({ error: error.message })})
-
-    
+      [company_id]
+    )
+    .then((result) => {
+      if (result.rowCount == 0) {
+        res.status(404).json("The selected company has no Tickets");
+      } else {
+        res.status(200).json(result);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 };
 
 /*********************___Get all the Tickets from All Users___*************************/
 export const getTicketsFromAllUsers = async (req, res) => {
   const { orderBy, ascOrDesc } = req.params;
 
-    await pool
-      .query(
-        `
+  await pool
+    .query(
+      `
     SELECT 
     t.id as "ticket_id", t.subject, t.content, t.created_at, t.completed_at, t.img_url,
     u.id AS "user_id", u.first_name, u.last_name, u.username, u.email,
@@ -246,49 +260,52 @@ export const getTicketsFromAllUsers = async (req, res) => {
     ON t.status_id = s.id
     ORDER BY ${orderBy} ${ascOrDesc}
     `
-      )
-      .then((results) => {
-        if (results.rowCount === 0) {
-          res.status(404).json("Ther s no Tickets");
-        } else {
-          res.status(200).json(results);
-        }
-      })
-      .catch((error) => { res.status(500).json({ error: error.message }) 
+    )
+    .then((results) => {
+      if (results.rowCount === 0) {
+        res.status(404).json("Ther s no Tickets");
+      } else {
+        res.status(200).json(results);
+      }
     })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 };
 
 /*********************___Get Infos from a User___*************************/
 export const getCompanyInfos = async (req, res) => {
   const { company_id } = req.params;
 
-
-    await pool
-      .query(
-        `
+  await pool
+    .query(
+      `
       SELECT 
       name, adress AS "street", number AS "street number", zip, city, country, phone, email
       FROM company
       WHERE id = $1
       `,
-        [company_id]
-      ) 
-      .then((results) => {
-        if (results.rowCount === 0) {
-          res.status(404).json("Something went wrong!!");
-        } else {
-          res.status(200).json(results);
-        }
-      })
-      .catch((error) => {     res.status(500).json({ error: error.message }) })
+      [company_id]
+    )
+    .then((results) => {
+      if (results.rowCount === 0) {
+        res.status(404).json("Something went wrong!!");
+      } else {
+        res.status(200).json(results);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 };
 
- /*********************___ Get all Users ___*************************/
+/*********************___ Get all Users ___*************************/
 export const getAllUsers = async (req, res) => {
   const { orderBy, ascOrDesc } = req.params;
 
-  await pool.query(
-    `
+  await pool
+    .query(
+      `
       SELECT u.id AS "user_id", c.id AS "company_id", us.id AS "status_id", u.first_name AS "first_name", u.last_name AS "last_name", u.username, c.name AS "company", u.email, us.status, r.role
       FROM users u
       JOIN company c
@@ -299,22 +316,79 @@ export const getAllUsers = async (req, res) => {
       ON r.id = u.role_id
       ORDER BY ${orderBy} ${ascOrDesc}
     `
-  )
-  .then((results) => {
-    if (results.rowCount === 0) {
-      res.status(404).json("Something went wrong!! Plz try again later!");
-    } else {
-      res.status(200).json(results);
-    }
-  })
-  .catch((error) => { res.status(500).json({ error: error.message }) 
+    )
+    .then((results) => {
+      if (results.rowCount === 0) {
+        res.status(404).json("Something went wrong!! Plz try again later!");
+      } else {
+        res.status(200).json(results);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+};
+
+
+/*********************___Update User Infos Inkl Passwort___*************************/
+export const updateUserInfos = async (req, res) => {
+  const {user_id} = req.params;
+  const { 
+    first_name,
+    last_name,
+    username,
+    email,
+    company_id,
+    role_id,
+    status_id,
+  } = req.body;
+ console.log(first_name, last_name, email, company_id, role_id, user_id);
+  await pool.query(
+    `
+    UPDATE users u
+    SET
+    first_name = $1,
+    last_name = $2,
+    username = $3,
+    email = $4,
+    company_id = $5,
+    role_id = $6,
+    status_id = $7
+    WHERE u.id = $8 RETURNING *;
+    `, [first_name, last_name, username, email, company_id, role_id, status_id, user_id]
+    )
+    .then((results) => {
+      res.status(201).json(results);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: err.message });
+    });
+};
+
+/*********************___Update User Passwort___*************************/
+export const updateUserPassword = async(req, res) => {
+const { user_id} = req.params;
+const { password } = req.body;
+
+const hashedPassword = await bcrypt.hash(password, 10); // password hashing
+
+await pool.query(
+  `
+UPDATE users SET password = $1 WHERE id = $2 RETURNING *;
+  `, [hashedPassword, user_id]
+)
+.then((results) => {
+  res.status(201).json(results);
 })
-  
+.catch((err) => {
+  res.status(500).json({ err: err.message });
+});
 }
 
-  /*********************___Verify Session___*************************/
-  export const verifySession = (req, res) => {
-    console.log("verifysession Start")
-    res.status(200).send("Token successfully verified");
-    console.log("verifysession End")
-  };
+
+/*********************___Verify Session___*************************/
+export const verifySession = (req, res) => {
+  console.log("verifysession Start");
+  res.status(200).send("Token successfully verified");
+  console.log("verifysession End");
+};
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,8 +1,22 @@
 import { logIn } from "../controllers/logInOutController.js"
 import { Router } from "express";
 import { verifyToken } from "../middleware/verifyToken.js"
-import { getTicketsFromAllUsers, getCompanyInfos, getCompanyTickets, createNewUser, updateTicket, getAllCompanies, updateCompanysStatus, createNewCompany, getAllUsers } from "../controllers/adminController.js"
 import { verifySession } from "../controllers/adminController.js";
+import { 
+        getTicketsFromAllUsers,
+        getCompanyInfos, 
+        getCompanyTickets, 
+        createNewUser, 
+        updateTicket, 
+        getAllCompanies, 
+        updateCompanysStatus, 
+        createNewCompany, 
+        getAllUsers,
+        updateUserInfos,
+        updateUserPassword
+        } 
+        from "../controllers/adminController.js"
+
 
 const adminRouter = Router();
 adminRouter.route("/login").post(logIn);// success
@@ -13,6 +27,8 @@ adminRouter.route("/ticketsprocompany/:company_id").get(verifyToken, getCompanyT
 adminRouter.route("/infos/:company_id").get(verifyToken, getCompanyInfos).put(verifyToken, updateCompanysStatus) //success
 adminRouter.route("/adduser").post(verifyToken, createNewUser)//done checked -> success
 adminRouter.route("/users/:orderBy/:ascOrDesc").get(verifyToken, getAllUsers)
+adminRouter.route("/user/:user_id").put(verifyToken, updateUserInfos)
+adminRouter.route("/user/update-password/:user_id").put(verifyToken, updateUserPassword);
 adminRouter.get("/verify", verifyToken, verifySession);
 
 
